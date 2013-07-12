cfad2f8e20fe1a10e3671a0643d71b9669eb11f3
poc7667 <poc7667@gmail.com>
2013-07-12 11:11:15 +0800

    meta verison

--- isbn.py
+++ isbn.py
@@ -1,4 +1,13 @@
-__author__ = 'poc.hsu'
+# -*- coding: utf-8 -*-
+__author__ = 'Poc'
+import re,json,sys,httplib,requests
+_isbn_lst='4710947917212'
+_URL_PREFIX='http://search.books.com.tw/exep/prod_search.php?cat=all&key='
+
+def fetch_data(isbn):
+    req_url=_URL_PREFIX+_isbn_lst
+
+    pass
 
 def main():
     print("isbn test")
